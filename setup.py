7be5b459a1e14a24e7592bf1431c83a56141114f
5j9 <5j9@users.noreply.github.com>
2021-10-24 19:03:25 +0330

    release: v0.2.2.dev0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import dirname
 
 setup(
     name='fipiran',
-    version='0.2.1',
+    version='0.2.2.dev0',
     long_description=open(
         f'{dirname(__file__)}/README.rst', encoding='utf-8').read(),
     long_description_content_type='text/x-rst',
