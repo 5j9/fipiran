eafc6d7c03bbfb1892d88f3848bf7f5926a95e13
5j9 <5j9@users.noreply.github.com>
2022-12-01 16:17:04 +0330

    feat(codal): new function: profit_growth

--- fipiran/codal.py
+++ fipiran/codal.py
@@ -4,3 +4,8 @@ from . import _DataFrame, _fipiran, _read_html
 async def financial_ratios() -> _DataFrame:
     text = await _fipiran('Codal/Ratio')
     return _read_html(text)[0]
+
+
+async def profit_growth() -> _DataFrame:
+    text = await _fipiran('Codal/RoshdPos')
+    return _read_html(text)[0]
--- test/test_codal.py
+++ test/test_codal.py
@@ -1,10 +1,10 @@
 from numpy import dtype
-from fipiran.codal import financial_ratios
+from fipiran.codal import financial_ratios, profit_growth
 
 from test.aiohttp_test_utils import file
 
 
-@file('financial_ratios.json')
+@file('financial_ratios.html')
 async def test_financial_ratios():
     df = await financial_ratios()
     assert [*df.dtypes.items()] == [
@@ -21,4 +21,17 @@ async def test_financial_ratios():
         ('ROA', dtype('float64')),
         ('ROE', dtype('float64')),
         ('سودواقعی هر سهم', dtype('int64'))
+    ]
+
+
+@file('profit_growth.html')
+async def test_profit_growth():
+    df = await profit_growth()
+    assert [*df.dtypes.items()] == [
+        ('نماد', dtype('O')),
+        ('تاریخ انتشار', dtype('O')),
+        ('دوره', dtype('O')),
+        ('سود واقعی دوره', dtype('O')),
+        ('سود واقعی دوره قبل', dtype('O')),
+        ('% رشد', dtype('int64'))
     ]
